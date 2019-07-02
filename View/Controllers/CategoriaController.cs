cd821fb449827c743b537f6261b0b6afb45bb07d
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-07-02 16:00:35 -0300

    ControllerCategoria

--- View/Controllers/CategoriaController.cs
+++ View/Controllers/CategoriaController.cs
@@ -8,12 +8,64 @@ namespace View.Controllers
 {
     public class CategoriaController : Controller
     {
-
         private CategoriaRepository repositorio;
+
+        public CategoriaController()
+        {
+            repositorio = new CategoriaRepository();
+        }
         // GET: Categoria
         public ActionResult Index()
         {
+            List<Categoria> categorias = repositorio.Obtertodos("");
+            ViewBag.Categorias = categorias;
             return View();
         }
+
+        public ActionResult Cadastro()
+        {
+            CategoriaRepository categoriaRepository = new CategoriaRepository();
+            List<Categoria> categoria = categoriaRepository.ObterTodos();
+            ViewBag.Categorias = categorias;
+            return View();
+
+        }
+
+        public ActionResult Store(int idCategoria, string nome)
+        {
+            Categoria categoria = new Categoria();
+            categoria.IdCategoria = idCategoria;
+            categoria.nome = nome;
+            repositorio.Inserir(categoria);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repositorio.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Categoria categoria = repositorio.ObterPeloId(id);
+            ViewBag.Categoria = categoria;
+            CategoriaRepository categoriaRepository = new CategoriaRepository();
+            List<Categoria> categorias = categoriaRepository.ObterTodos();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome, int idCategoria)
+        {
+            Categoria categoria = new Categoria();
+            categoria.Id = id;
+            categoria.Nome = nome;
+            categoria.IdCategoria = idCategoria;
+            repositorio.Alterar(categoria);
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
