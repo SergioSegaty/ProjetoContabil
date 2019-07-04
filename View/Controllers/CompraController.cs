da684a44542add4de096e963dbb1d07b5df161c3
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-07-04 16:23:18 -0300

    Feito novo database. Feito Layout. Feito View Contabilidade. Categoria.Compra. Index.Cadastrar.Editar.

---
+++ View/Controllers/CompraController.cs
@@ -0,0 +1,73 @@
+﻿using Model;
+using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class CompraController : Controller
+    {
+        private CompraRepository repositorio;
+        public CompraController()
+        {
+            repositorio = new CompraRepository();
+        }
+        // GET: Compra
+        public ActionResult Index()
+        {
+            List<Compra> compras = repositorio.ObterTodos();
+            ViewBag.Compras = compras;
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            CompraRepository compraRepository = new CompraRepository();
+            List<Compra> compras = compraRepository.ObterTodos();
+            ViewBag.Compras = compras;
+            return View();
+        }
+
+        public ActionResult Store(int idCompra, int idCartaoCredito, decimal valor, DateTime dataCompra)
+        {
+            Compra compra = new Compra();
+            compra.CartaoCredito.Id = idCartaoCredito;
+            compra.Valor = valor;
+            compra.DataCompra = dataCompra;
+            repositorio.Inserir(compra);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repositorio.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Compra compra = repositorio.ObterPeloId(id);
+            ViewBag.Compra = compra;
+            CompraRepository compraRepository = new CompraRepository();
+            List<Compra> compras = compraRepository.ObterTodos();
+            ViewBag.Compra = compras;
+            return View();
+        }
+
+        public ActionResult Update(int idCompra, int idCartaoCredito, decimal valor, DateTime dataCompra)
+        {
+            Compra compra = new Compra();
+            compra.Id = idCompra;
+            compra.CartaoCredito.Id = idCartaoCredito;
+            compra.Valor = valor;
+            compra.DataCompra = dataCompra;
+            repositorio.Alterar(compra);
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
